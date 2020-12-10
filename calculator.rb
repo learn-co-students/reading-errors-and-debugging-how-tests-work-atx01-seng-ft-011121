d7bc89c5980cebcde3783e6207d68142654a192a
Jesse Brown <brownjesse8895@yahoo.com>
2020-12-10 20:46:26 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
-# Add your variables here
+first_number = 10 
+second_number = 5
+def first_number
+  puts first_number + second_number
+end
+
+first_number
+
+sum = first_number + second_number 
+ difference = first_number - second_number 
+ product = first_number * second_number 
+ quotient = first_number / second_number 
+
+
+
