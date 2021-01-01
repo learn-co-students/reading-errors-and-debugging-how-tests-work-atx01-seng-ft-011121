7072e16161396f6f88c870404c7262f65bf8181b
David Jun <djun301@gmail.com>
2021-01-01 04:57:19 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = "Hello world!".to_i
+second_number = "123".to_i
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
